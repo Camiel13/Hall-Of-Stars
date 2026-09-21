adb28c99df60964b379ca211238ca953f809ca6a
Camiel13 <camielvanderlocht@gmail.com>
2026-09-21 12:37:22 +0200

    v4.1.0 - Code refactoring

--- hallofstars.py
+++ hallofstars.py
@@ -2,7 +2,7 @@ import sys
 import pygame
 import settings
 from level import Level
-from ui import WelcomeScreen, Dialogue
+from ui import UISequence, create_welcome_sequence
 
 class Game:
     def __init__(self):
@@ -22,9 +22,7 @@ class Game:
         # Create the world
         self.level = Level(display_surface=self.display_surface)
         self.state = "PLAYING" if self.level.api.stardance_username else "WELCOMING"
-        self.welcome_screen = None
-        if self.state == "WELCOMING":
-            self.welcome_screen = WelcomeScreen()
+        self.welcome_sequence = create_welcome_sequence()
         
     def run(self):
         while self.running:
@@ -34,17 +32,21 @@ class Game:
                     self.running = False
                 
                 if self.state == "WELCOMING":
-                    self.welcome_screen.handle_event(event)
-                    if self.welcome_screen.is_finished:
+                    self.welcome_sequence.handle_event(event)
+                    if self.welcome_sequence.is_finished:
                         self.state = "PLAYING"                        
-                        self.level.api.set_username(self.welcome_screen.username)
+                        self.level.api.set_username(self.welcome_sequence.results["username"])
                         self.level.hallway.unload(camera=self.level.camera_group, obstacles=self.level.obstacles)
                         self.level.generate_hallway()
-                        self.welcome_screen = None
+                        self.welcome_sequence = None
+                else:
+                    self.welcome_sequence = None
                         
             # Tick Logic
             if self.state == "PLAYING":
                 self.level.tick()
+            elif self.state == "WELCOMING":
+                self.welcome_sequence.update()
         
             # Rendering
             self.display_surface.fill(settings.COLORS["background"])
@@ -57,7 +59,7 @@ class Game:
             self.screen.blit(scaled_surface, (0, 0))
             
             if self.state == "WELCOMING":
-                self.welcome_screen.draw(self.screen)
+                self.welcome_sequence.draw(self.screen)
             
             pygame.display.flip()
             self.clock.tick(settings.FPS)
--- ui.py
+++ ui.py
@@ -1,9 +1,64 @@
 import pygame
 import textwrap
+from abc import ABC, abstractmethod
 
-class InputBox:
-    def __init__(self, placeholder: str = "Enter text here...", question: str = "Input your answer..."):
+class UIElement(ABC):
+    def __init__(self):
+        self.is_finished = False
+        self.value = None
+    
+    @abstractmethod
+    def handle_event(self, event):
+        pass
+
+    @abstractmethod
+    def draw(self, surface: pygame.Surface):
+        pass
+    
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def update(self):
+        pass
+    
+    def _draw_background_blur(self, surface: pygame.Surface, alpha: int = 170):
+        overlay = pygame.Surface((surface.get_width(), surface.get_height()), pygame.SRCALPHA)
+        overlay.fill((0, 0, 0, 170))
+        surface.blit(overlay, (0, 0))
+        
+    def _draw_box(
+        self, 
+        surface: pygame.Surface, 
+        rect: pygame.Rect, 
+        bg_color: tuple[int, int, int],
+        border_color: tuple[int, int, int],
+        border_width = 5,
+        border_radius: int = 12
+    ):
+        pygame.draw.rect(surface, bg_color, rect, border_radius=border_radius)
+        pygame.draw.rect(surface, border_color, rect, width=border_width, border_radius=border_radius)
+        
+    def _draw_text(
+        self,
+        surface: pygame.Surface, 
+        font: pygame.font.Font,
+        text: str,
+        color: tuple[int, int, int],
+        **rect_kwargs
+    ):
+        surf = font.render(text, True, color)
+        rect = surf.get_rect(**rect_kwargs)
+        surface.blit(surf, rect)
+        return rect
+
+class InputScreen(UIElement):
+    def __init__(self, placeholder: str = "Enter text here...", question: str = "Input your answer...", key: str = None):
+        super().__init__()
         self.value = ""
+        self.key = key
         self.placeholder = placeholder
         self.question = question
 
@@ -19,10 +74,12 @@ class InputBox:
         self.placeholder_color = (178, 183, 198)
         self.border_color = (232, 224, 176)
         
-    def handle_event(self, event) -> str | None:
+    def handle_event(self, event):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RETURN:
-                return self.value.strip()
+                if self.value:
+                    self.value = self.value.strip()
+                    self.is_finished = True
             elif event.key == pygame.K_BACKSPACE:
                 self.value = self.value[:-1]
             else:
@@ -31,20 +88,15 @@ class InputBox:
     
     def draw(self, surface: pygame.Surface):
         # Draw blur
-        overlay = pygame.Surface((surface.get_width(), surface.get_height()), pygame.SRCALPHA)
-        overlay.fill((0, 0, 0, 170))
-        surface.blit(overlay, (0, 0))
+        self._draw_background_blur(surface=surface)
         
         # Draw the rect
         input_field = pygame.Rect(0, 0, 400, 50)
         input_field.center = (surface.get_width() // 2, (surface.get_height() // 2) + 40)
-        pygame.draw.rect(surface, self.bg_color, input_field, border_radius=10)
-        pygame.draw.rect(surface, self.border_color, input_field, width=5, border_radius=10)
+        self._draw_box(surface=surface, rect=input_field, bg_color=self.bg_color, border_color=self.border_color)
         
         # Add the question
-        question_surf = self.font.render(self.question, True, self.question_color)
-        question_rect = question_surf.get_rect(center=(surface.get_width() // 2, (surface.get_height() // 2) - 40))
-        surface.blit(question_surf, question_rect)
+        self._draw_text(surface=surface, font=self.font, text=self.question, color=self.question_color, center=(surface.get_width() // 2, (surface.get_height() // 2) - 40))
         
         # Render typed text or placeholder
         if self.value:
@@ -56,10 +108,10 @@ class InputBox:
         text_rect = text_surface.get_rect(midleft=(input_field.x + 12, input_field.centery))
         surface.blit(text_surface, text_rect)
         
-class Dialogue:
+class Dialogue(UIElement):
     def __init__(self, text: str, character_path: pygame.Surface = None, character_name: str = None):
+        super().__init__()
         self.text = text
-        self.character_sprite = pygame.image.load(character_path).convert_alpha()
         self.character_name = character_name
         self.sound = pygame.mixer.Sound("assets/audio/cartoon-yap.mp3")
         
@@ -79,36 +131,25 @@ class Dialogue:
         self.font = pygame.font.SysFont("z003", 25)
         self.name_font = pygame.font.SysFont("z003", 25, italic=True)
         
-    def start(self):
-        self.visible_characters = 0
-        self.is_talking = True
-        self.sound.play(loops=-1)
-        self.lines = textwrap.wrap(self.text, width=48)
-        
-    def update(self):
-        if self.is_talking:
-            if self.visible_characters < len(self.text):
-                self.visible_characters += 1
-            else:
+    def handle_event(self, event):
+        if event.type == pygame.KEYDOWN and (event.key == pygame.K_SPACE or event.key == pygame.K_RETURN):
+            if self.is_talking:
+                self.visible_characters = len(self.text)
                 self.is_talking = False
                 self.sound.stop()
-        
+            else:
+                self.is_finished = True
     
     def draw(self, surface: pygame.Surface):
         # Create the background box
-        box_width = surface.get_width() // 2
-        box_height = surface.get_height() // 2
-        self.box = pygame.Rect(0, 0, box_width, box_height)
-        self.box.center = (surface.get_width() // 2, surface.get_height() // 2)
+        box = pygame.Rect(0, 0, surface.get_width() // 2, surface.get_height() // 2)
+        box.center = (surface.get_width() // 2, surface.get_height() // 2)
 
         # Draw blur
-        overlay = pygame.Surface((surface.get_width(), surface.get_height()), pygame.SRCALPHA)
-        overlay.fill((0, 0, 0, 170))
-        surface.blit(overlay, (0, 0))
+        self._draw_background_blur(surface=surface)
         
         # Draw box and border
-        pygame.draw.rect(surface, self.bg_color, self.box, border_radius=12)
-        pygame.draw.rect(surface, self.border_color, self.box, width=5, border_radius=12)
+        self._draw_box(surface=surface, rect=box, bg_color=self.bg_color, border_color=self.border_color)
         
         # Draw the character sprite if present
         character_padding = 50
@@ -120,7 +161,7 @@ class Dialogue:
             
         # Write text
         line_height = 35
-        start_x, start_y = self.box.x + 32, self.box.y + 32
+        start_x, start_y = box.x + 32, box.y + 32
         
         remaining = self.visible_characters
         for i, line in enumerate(self.lines):
@@ -135,18 +176,73 @@ class Dialogue:
             
         if self.character_name:
             name_surf = self.name_font.render(f"~ {self.character_name} ", True, self.text_color)
-            name_rect = name_surf.get_rect(bottomleft=(self.box.left + 35, self.box.bottom - 35))
+            name_rect = name_surf.get_rect(bottomleft=(box.left + 35, box.bottom - 35))
             surface.blit(name_surf, name_rect)
+         
+    def update(self):
+        if self.is_talking:
+            if self.visible_characters < len(self.text):
+                self.visible_characters += 1
+            else:
+                self.is_talking = False
+                self.sound.stop()       
             
-        self.update()
+    def start(self):
+        self.visible_characters = 0
+        self.is_talking = True
+        self.sound.play(loops=-1)
+        self.lines = textwrap.wrap(self.text, width=48)
     
     def stop(self):
         self.is_talking = False
-        self.sound.stop()        
+        self.sound.stop()      
 
-class WelcomeScreen:
-    def __init__(self):
-        self.pages = [
+class UISequence:
+    def __init__(self, steps: list[UIElement]):
+        self.steps = steps
+        self.current_index = 0
+        self.is_finished = False
+        self.results = {}
+        
+        if self.steps:
+            self.current_step.start()
+
+    @property
+    def current_step(self):
+        return self.steps[self.current_index]
+        
+    def handle_event(self, event) -> str | None:
+        if self.is_finished:
+            return
+
+        # Let the current step handle the input event
+        self.current_step.handle_event(event=event)
+        
+        if self.current_step.is_finished:
+            self.current_step.stop()
+            
+            if self.current_step.value and getattr(self.current_step, "key", None):
+                self.results[self.current_step.key] = self.current_step.value
+                
+            self.current_index += 1
+            if self.current_index >= len(self.steps):
+                self.is_finished = True
+            else:
+                self.current_step.start()
+                
+    def draw(self, surface: pygame.Surface):
+        if not self.is_finished:
+            self.current_step.draw(surface=surface)
+    
+    def update(self):
+        if not self.is_finished:
+            self.current_step.update()
+
+
+
+def create_welcome_sequence() -> UISequence:
+    return UISequence(
+        steps=[
             Dialogue(
                 text="Welcome to The Hall of Stars. You can move with WASD or Arrow Keys. Doors automatically bring you into the new room. You can interact with NPC's with 'E'. Use 'Space' or 'Enter' to navigate to the next page.",
                 character_path="assets/characters/standing_star.png",
@@ -157,44 +253,15 @@ class WelcomeScreen:
                 character_path="assets/logo/star_with_flag.png",
                 character_name="Starling"
             ),
-            InputBox(
+            InputScreen(
                 placeholder="Enter your username...",
-                question="What is your username on Stardance?"
+                question="What is your username on Stardance?",
+                key="username"
             ),
             Dialogue(
-                text="Time to wander around the Hall of Stars username... Have fun!",
+                text="Time to wander around the Hall of Stars... Have fun!",
                 character_path="assets/characters/dancing_star.png",
                 character_name="Starling"
             ),
         ]
-        self.pages[0].start()
-        self.current_page = 0
-        self.is_finished = False
-        self.username = ""
-        
-    def handle_event(self, event) -> str | None:
-        if self.current_page == 0:
-            if event.type == pygame.KEYDOWN and (event.key == pygame.K_SPACE or event.key == pygame.K_RETURN):
-                self.current_page = 1
-                self.pages[0].stop()
-                self.pages[1].start()
-        elif self.current_page == 1:    
-            if event.type == pygame.KEYDOWN and (event.key == pygame.K_SPACE or event.key == pygame.K_RETURN):
-                self.current_page = 2
-                self.pages[1].stop()
-        elif self.current_page == 2:
-            input_box = self.pages[2]
-            entered_username = input_box.handle_event(event)
-            if entered_username:
-                self.username = entered_username
-                self.current_page = 3
-                self.pages[3].text = self.pages[3].text.replace("username", entered_username)
-                self.pages[3].start()
-        elif self.current_page == 3:
-            if event.type == pygame.KEYDOWN and (event.key == pygame.K_SPACE or event.key == pygame.K_RETURN):
-                self.pages[3].stop()
-                self.is_finished = True
-                
-    def draw(self, surface: pygame.Surface):
-        current_component = self.pages[self.current_page]
-        current_component.draw(surface)
+    )
